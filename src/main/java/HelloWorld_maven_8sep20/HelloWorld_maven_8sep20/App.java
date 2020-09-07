d73e5855adb05ce13877c080d8c0a6c8a3ec4ab2
sangit85 <santoshk.github@gmail.com>
2020-09-07 11:23:40 +0530

    HelloWorldCommit_7sep

---
+++ src/main/java/HelloWorld_maven_8sep20/HelloWorld_maven_8sep20/App.java
@@ -0,0 +1,13 @@
+package HelloWorld_maven_8sep20.HelloWorld_maven_8sep20;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
